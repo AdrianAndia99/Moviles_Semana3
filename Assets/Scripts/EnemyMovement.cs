be26694b569934a6d233c6e572f0985d9cc51d48
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-04 11:34:30 -0500

    Dont Collision with wall

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
