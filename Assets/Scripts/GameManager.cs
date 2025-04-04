4894e85ee6cbefd360e7c5220d8c6eb09abcdf66
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-04 01:12:24 -0500

    el ingles se fue de sabatico en este commit
    error cochino, quede

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameData gameData;
-    //[SerializeField] private PlayerController player;
+    [SerializeField] private PlayerController player;
     [SerializeField] private ScoreManager scoreManager;
     [SerializeField] private HealthManager healthManager;
 
     private void Start()
     {
        ShipData selectedShip = gameData.selectedShip;
+       player.SetShip(gameData.selectedShip);
 
         if (selectedShip != null)
         {
