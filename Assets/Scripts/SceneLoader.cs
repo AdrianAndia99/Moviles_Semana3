875716009924dcdb593452b010731e7d0fa85e03
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-05 16:11:56 -0500

    Random Scenes

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,9 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private string[] randomScenes;
+
+
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadRandomScene()
+    {
+        int randomIndex = Random.Range(0, randomScenes.Length);
+        SceneManager.LoadScene(randomScenes[randomIndex]);
     }
 
     public void QuitGame()
