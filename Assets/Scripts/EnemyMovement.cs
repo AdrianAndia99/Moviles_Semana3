d55a14dfd084ceb8af415c52fc38c62f9435d508
maradw <maria.mendoza@tecsup.edu.pe>
2025-04-08 17:33:16 -0500

    enemies pooling

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
-
+    [SerializeField]
+    private SimpleObjectPooling obstacleType;
+    [SerializeField] private Rigidbody2D myRigidbody;
     private void Update()
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
@@ -15,14 +17,44 @@ public class EnemyMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Wall"))
         {
             Debug.Log("Enemy collide");
-            Destroy(this.gameObject);
+            isSetUp = false;
+            obstacleType.ObjectReturn(this.gameObject);
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Destroy(this.gameObject);
+            isSetUp = false;
+            obstacleType.ObjectReturn(this.gameObject);
         }
     }
+    private bool isSetUp;
+
+    private void OnEnable()
+    {
+        obstacleType.onEnableObject += SetUp;
+    }
+
+    private void OnDisable()
+    {
+        obstacleType.onEnableObject -= SetUp;
+    }
+
+    private void SetUp()
+    {
+        if (!isSetUp)
+        {
+            //TO DO SET UP
+            if (myRigidbody == null)
+            {
+                myRigidbody = GetComponent<Rigidbody2D>();
+            }
+
+            myRigidbody.linearVelocity = Vector2.left * speed;
+
+            isSetUp = true;
+        }
+    }
+
 }
