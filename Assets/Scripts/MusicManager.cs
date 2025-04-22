93af8e38ae61ad117d07d575201ebaf6ffce9db0
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-22 16:22:02 -0500

    AUDIO SET

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private AudioClipSO clipMenu;
+    [SerializeField] private AudioClipSO clipSelection;
+    [SerializeField] private AudioClipSO clipGamePlay;
+
+    private void Start()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        if (currentScene == "MainMenu")
+        {
+            clipMenu.PlayLoop();
+        }
+        else if (currentScene == "CharacterSelection")
+        {
+            clipSelection.PlayLoop();
+        }
+        else if(currentScene == "MainGameGyroscope")
+        {
+            clipGamePlay.PlayLoop();
+        }
+    }
+}
